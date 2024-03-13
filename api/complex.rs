2b01ab780f422284b98e557fdc602bc68a2a84fe
ellie-sleightholm <code.ot.future@gmail.com>
2024-03-13 21:04:44 +0000

    updating all docstrings

--- api/complex.rs
+++ api/complex.rs
@@ -6,17 +6,46 @@ use vercel_runtime::{
     Request, RequestPayloadExt, Response, ServiceBuilder, StatusCode,
 };
 
+/// Represents the payload structure expected in the incoming request.
 #[derive(Debug, Serialize, Deserialize)]
 struct Payload {
+    /// The name included in the payload.
     name: String,
 }
 
+/// Represents an error response in the API.
 #[derive(Serialize)]
 pub struct APIError {
+    /// A message describing the error.
     pub message: &'static str,
+    /// A code representing the error.
     pub code: &'static str,
 }
 
+/// The main entry point of the application.
+///
+/// Initializes the logger and runs the service.
+///
+/// # Returns
+///
+/// Returns `Ok(())` if the application runs successfully.
+///
+/// # Errors
+///
+/// Returns an error if there's an issue during runtime.
+///
+/// # Examples
+///
+/// ```rust
+/// use vercel_runtime::Error;
+/// use simple_runtime_demo::handler;
+///
+/// #[tokio::main]
+/// async fn main() -> Result<(), Error> {
+///     handler().await?;
+///     Ok(())
+/// }
+/// ```
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
@@ -34,6 +63,26 @@ async fn main() -> Result<(), Error> {
     run_service(handler).await
 }
 
+/// Handles incoming requests to the API.
+///
+/// # Arguments
+///
+/// * `req` - A `Request` object representing the incoming HTTP request.
+///
+/// # Returns
+///
+/// Returns a `Result` containing either a `Response` if the request is successful, or an `Error` if there's an issue.
+///
+/// # Examples
+///
+/// ```rust
+/// use vercel_runtime::{Request, Response, Body, Error};
+/// use simple_runtime_demo::handler;
+///
+/// async fn handle_request(req: Request) -> Result<Response<Body>, Error> {
+///     handler(req).await
+/// }
+/// ```
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     tracing::info!("Choosing a starter Pokemon");
     let payload = req.payload::<Payload>();
